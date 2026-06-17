0b8dfcb002dc84b337fcecd14327d8658ec5a8b3
Igor <lonelyytgames@gmail.com>
2026-06-18 00:39:37 +0200

    fixxxxx

--- backend/main.py
+++ backend/main.py
@@ -4681,4 +4681,4 @@ if __name__ == "__main__":
     import os
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port, ws="websockets")  # ← DODAJ ws="websockets"
